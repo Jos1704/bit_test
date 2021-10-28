38c0b78482cf9bb54d1eef53a50948e7dc7250da
Josue Patiño <josuesalvador093@gmail.com>
2021-10-28 13:50:46 -0500

    .

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ $('#search_date').click(function() {
     var dias = 8; // Número de días a agregar
     var container  = $('#container');
     fecha.setDate(fecha.getDate() + dias);
-    console.log(fecha);
     container.html('la fecha es: '+fecha);
 })
