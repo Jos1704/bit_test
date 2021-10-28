03681a0ea43a87544c85ca4f4a1959112ab6a05a
Robert <robertcasti56@gmail.com>
2021-10-28 14:29:35 -0500

    Funcion fecha

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ $('#search_date').click(function() {
     var fecha = new Date($('#date_init').val());
     var dias = 8; // Número de días a agregar
     var container  = $('#container');
+    var container2  = $('#container2');
+
     fecha.setDate(fecha.getDate() + dias);
-    container.html('la fecha es: '+fecha);
+    container.html('la fecha Domingo de ramos es: ' + fecha);
+
+    fecha.setDate(fecha.getDate() + dias);
+    container2.html('la fecha Domingo de resureccion es: ' + fecha);
+
+    fecha = "";
 })
